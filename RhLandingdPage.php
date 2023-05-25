546e1b2d1ff435bce3b7b85f44a57fc953aa7477
kassimi youssef <youssef.kassimi-etu@etu.univh2c.ma>
2023-05-25 20:51:43 +0100

    Rh Landing Page (front)

---
+++ RhLandingdPage.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="includes/RhMenu.css">
+    <link rel="stylesheet" href="includes/RhLandingPage.css">
+    <style>
+
+    </style>
+</head>
+
+<body>
+    <?php
+    include('includes/RhMenu.html');
+    ?>
+    <div class="card-container">
+
+        <div class="row">
+            <a href="#">
+                <div class="card">
+                    <h3 class="card-heading">card heading</h3>
+                    <p class="card-body">card body</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="card">
+                    <h3 class="card-heading">card heading</h3>
+                    <p class="card-body">card body</p>
+                </div>
+            </a>
+
+        </div>
+        <div class="row">
+            <a href="">
+                <div class="card">
+                    <h3 class="card-heading">card heading</h3>
+                    <p class="card-body">card body</p>
+                </div>
+            </a>
+            <a href="">
+                <div class="card">
+                    <h3 class="card-heading">card heading</h3>
+                    <p class="card-body">card body</p>
+                </div>
+            </a>
+
+
+        </div>
+    </div>
+</body>
+
+</html>
