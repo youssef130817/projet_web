d1bef5f6fe5952b790737fd9f605c552a2482d67
kassimi youssef <youssef.kassimi-etu@etu.univh2c.ma>
2023-05-25 17:26:45 +0100

    haha

---
+++ includes/infoverification.js
@@ -0,0 +1,40 @@
+console.log('JavaScript file loaded');
+//déclaration des variables et obtentions des éléments html 
+var enter = document.querySelector('.Entrer');
+var errmessage = document.querySelector('.error');
+var user = document.querySelector('#user');
+var pass = document.querySelector('#pass');
+
+//définition des expressions régulières pour l'email et le mot de pass
+var Eregex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z]{2,})$/;
+var Pregex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9\W]).{8,}$/;
+
+//ajouter un evenemnt pour le champ de l'eamil à chaque caractère saisie
+user.addEventListener('keyup', function() {
+    var isValidE = Eregex.test(user.value);
+    if (isValidE)
+        user.style.border = '1px solid green';
+    else
+        user.style.border = '1px solid red';
+})
+
+//ajouter un evenemnt pour le champ de mot de passe à chaque caractère saisie
+pass.addEventListener('keyup', function() {
+    var isValidP = Pregex.test(pass.value);
+    if (isValidP)
+        pass.style.border = '1px solid green';
+    else
+        pass.style.border = '1px solid red';
+})
+
+//ajouter un evenemnt pour boutton entrer pour vérifier l'email et le mot de passe 
+enter.addEventListener('click', function(event) {
+    let isValidE = Eregex.test(user.value);
+    let isValidP = Pregex.test(pass.value);
+    event.preventDefault();
+    if (!isValidE || !isValidP) {
+        errmessage.style.display = 'block';
+        errmessage.textContent = 'veuillez remplir les champs avec des informations valides !!';
+    } else
+        errmessage.style.display = 'none';
+})
--- index.php
+++ index.php
@@ -1,67 +1,60 @@
 <?php
-    session_start();
-    include('connect.php');
-    if(!isset($_SESSION['cmpt'])) $_SESSION['cmpt']=0;
-    if(isset($_POST['cnx']))
-    {
-        $l=$_POST['email'];
-        $p=$_POST['pass'];
-        $req=$bdd->prepare("SELECT * from comptes WHERE login='$l' AND password='$p'");
-        $req->execute();
-        $result=$req->fetchAll(PDO::FETCH_ASSOC);
-        if($req->rowCount()==1)
-        {
-            $_SESSION['Auth']=$l;
-            unset($_SESSION['cmpt']);
-            //header('location:');
-        }
-        else 
-        {
-            $e="err";
-            $_SESSION['cmpt']++;
-        }
+session_start();
+include('connect.php');
+if (!isset($_SESSION['cmpt'])) $_SESSION['cmpt'] = 0;
+if (isset($_POST['login'])) {
+    $l = $_POST['username'];
+    $p = $_POST['password'];
+    $req = $bdd->prepare("SELECT * from comptes WHERE login='$l' AND password='$p'");
+    $req->execute();
+    $result = $req->fetchAll(PDO::FETCH_ASSOC);
+    if ($req->rowCount() == 1) {
+        $_SESSION['Auth'] = $l;
+        unset($_SESSION['cmpt']);
+        //header('location:');
+    } else {
+        $e = "err";
+        $_SESSION['cmpt']++;
     }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../projet_web/css/bootstrap.css">
-    <link rel="stylesheet" href="../projet_web/includes/style.css">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="includes/style.css">
+
 </head>
 
-<body style="background-color: #D3D3D3;">
-    <?php
-        include('../projet_web/includes/menu.html');
-    ?>
-    <div class="row">
-        <div class="col-md-8">
-            <img src="../projet_web/images/img22.png" alt="" id="imgauth"
-                style="position: absolute; top: 100px; left: 100px; width: 50%; height:100%;">
+<body>
+    <form class="form-container">
+        <h3>S'Authentifier</h3>
+        <div class="form-grp">
+            <label for="username">Email </label><br>
+            <input type="email" name="username" placeholder="xxxxx@domain" id="user">
         </div>
-        <?php
-        include('../projet_web/includes/form.html');
-      ?>
-    </div>
+        <div class="form-grp">
+            <label for="password">mot de pass </label><br>
+            <input type="password" name="password" placeholder="********" id="pass">
+            <p class="error"></p>
+        </div>
+        <div class="form-grp">
+            <input type="submit" class="Entrer" name="login" value="Entrer">
+        </div>
+    </form>
+    <img src="../projet_web/images/img22.png" alt="" id="imgauth">
+    <script src="includes/infoverification.js"></script>
 </body>
 <?php
-    if(isset($e))
-    {
-        if($_SESSION['cmpt']==3) 
-        {
-            unset($_SESSION['cmpt']);
-            header('location:reinit.php');
-        }
-        ?>
-<div>
-    <p class="errauth">Verifiez vos coordonnées d'authentification!!</p>
-</div>
-<?php 
-    }?>
+if (isset($e)) {
+    if ($_SESSION['cmpt'] == 3) {
+        unset($_SESSION['cmpt']);
+        header('location:reinit.php');
+    }
+?>
+<?php
+} ?>
 
 </html>
