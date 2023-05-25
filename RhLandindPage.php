e232b9ab922d959be5284d8778ec27db49e24954
kassimi youssef <youssef.kassimi-etu@etu.univh2c.ma>
2023-05-25 19:32:05 +0100

    menu rh

---
+++ RhLandindPage.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="includes/RhMenu.css">
+</head>
+
+<body>
+    <?php
+    include('includes/RhMenu.html');
+    ?>
+</body>
+
+</html>
