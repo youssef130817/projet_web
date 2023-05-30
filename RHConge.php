1220ebbbf21f73790e986be8aa97c54690804f00
kassimi youssef <youssef.kassimi-etu@etu.univh2c.ma>
2023-05-30 17:51:43 +0100

    conge

--- RHConge.php
+++ RHConge.php
@@ -10,65 +10,64 @@
     <?php
     include('includes/RhMenu.html');
     ?>
-    <main class="table">
-        <section class="table-header">
-            <h3>Demandes de congés</h3>
-        </section>
-        <section class="table-body">
-            <table>
+    <form>
+        <main class="table">
+            <section class="table-header">
+                <h3>Demandes de congés</h3>
+            </section>
+            <section class="table-body">
+                <table>
 
-                <thead>
-                    <tr>
-                        <td>Nom</td>
-                        <td>Prénom</td>
-                        <td>Date Début</td>
-                        <td>Date Fin</td>
-                        <td>Etat</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Nom</td>
-                        <td>Prénom</td>
-                        <td>Date Début</td>
-                        <td>Date Fin</td>
-                        <td class="Etat">
-                            <button id="btnA"></button>
-                            <button id="btnR"></button>
-                            <label class="label_coment" for="comment">Commentaire</label>
-                            <input class="comment" type="text" name="comment" placeholder="si vous reffuser laisser un commentaire !!">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Nom</td>
-                        <td>Prénom</td>
-                        <td>Date Début</td>
-                        <td>Date Fin</td>
-                        <td class="Etat">
-                            <button id="btnA"></button>
-                            <button id="btnR"></button>
-                            <label class="label_coment" for="comment">Commentaire</label>
-                            <input type="text" class="comment" name="comment" placeholder="si vous reffuser laisser un commentaire !!">
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>Nom</td>
-                        <td>Prénom</td>
-                        <td>Date Début</td>
-                        <td>Date Fin</td>
-                        <td class="Etat">
-                            <button id="btnA"></button>
-                            <button id="btnR"></button>
-                            <label class="label_coment" for="comment">Commentaire</label>
-                            <input type="text" class="comment" placeholder="si vous reffuser laisser un commentaire !!" name="comment">
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </section>
-
-    </main>
-
+                    <thead>
+                        <tr>
+                            <td>Nom</td>
+                            <td>Prénom</td>
+                            <td>Date Début</td>
+                            <td>Date Fin</td>
+                            <td>Etat</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        require 'connect.php';
+                        $state = 'en cours';
+                        $result = $bdd->query("SELECT * FROM `conges`, `employe` WHERE conges.ID_EMP = employe.ID_EMP AND conges.STATUT_CONGES='$state'");
+                        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+                            echo '<tr>';
+                            echo '<td>' . $row['NOM_EMP'] . '</td>';
+                            echo '<td>' . $row['PRENOM_EMP'] . '</td>';
+                            echo '<td>' . $row['DATE_DEBUT_CONGE'] . '</td>';
+                            echo '<td>' . $row['DATE_FIN_CONGE'] . '</td>';
+                            echo ' <form action="RhConge.php" method="post">';
+                            echo '  <td>
+                                        <input type="submit" name="accepter" id="btnA" value="">
+                                        <input type="submit" name="reffuser" id="btnR" value="">
+                                        <label class="label_coment" for="comment">Commentaire</label>
+                                        <p class="error"></p>
+                                        <input type="hidden" name="idc" value="' . $row["ID_EMP"] . '">
+                                    </td>';
+                            echo '</form>';
+                            echo '</tr>';
+                        }
+                        if (isset($_POST["accepter"])) {
+                            $id = $_POST["idc"];
+                            $state = 'validé';
+                            $req = $bdd->prepare("UPDATE `conges` set STATUT_CONGES='$state' WHERE ID_EMP='$id'");
+                            $req->execute();
+                        }
+                        if (isset($_POST["reffuser"])) {
+                            $id = $_POST["idc"];
+                            $state = 'reffuser';
+                            $req = $bdd->prepare("UPDATE `conges` set STATUT_CONGES='$state' WHERE ID_EMP='$id'");
+                            $req->execute();
+                        }
+                        ?>
+                        </tr>
+                    </tbody>
+                </table>
+            </section>
+        </main>
+    </form>
 </body>
 
 </html>
