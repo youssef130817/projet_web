7f0491d6f262d7bc650f63ac40fee3d66d46e02e
kassimi youssef <youssef.kassimi-etu@etu.univh2c.ma>
2023-06-07 15:47:36 +0100

    v

--- RHConge.php
+++ RHConge.php
@@ -3,14 +3,11 @@ session_start();
 if (!isset($_SESSION['Auth'])) {
     header('location: index.php');
 }
-echo "<p>Bonjour " . $_SESSION['Auth']['nom_emp'] . " </p>";
-// print_r($_SESSION);
 ?>
 <!DOCTYPE html>
 <html>
 
 <head>
-    <link rel="stylesheet" href="includes/RhMenu.css">
     <link rel="stylesheet" href="includes/RhConge.css">
 </head>
 
@@ -21,10 +18,10 @@ echo "<p>Bonjour " . $_SESSION['Auth']['nom_emp'] . " </p>";
     ?>
     <form>
         <main class="table">
-            <section class="table-header">
+            <div class="table-header">
                 <h3>Demandes de congés</h3>
-            </section>
-            <section class="table-body">
+            </div></br>
+            <div class="table-body">
                 <table>
                     <thead>
                         <tr>
@@ -33,26 +30,29 @@ echo "<p>Bonjour " . $_SESSION['Auth']['nom_emp'] . " </p>";
                             <td>Date Début</td>
                             <td>Date Fin</td>
                             <td>Etat</td>
+                            <td>Traiter</td>
                         </tr>
                     </thead>
+
                     <tbody>
                         <?php
                         require 'connect.php';
-                        $state = 'en cours';
-                        $result = $bdd->query("SELECT * FROM `conges`, `employee` WHERE conges.id_emp = employee.id_emp AND conges.statut_cg='$state'");
+                        $result = $bdd->query("SELECT * FROM `conges`, `employee` WHERE conges.id_emp = employee.id_emp");
                         while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
                             echo '<tr>';
                             echo '<td>' . $row['nom_emp'] . '</td>';
                             echo '<td>' . $row['prenom_emp'] . '</td>';
                             echo '<td>' . $row['date_debut_cg'] . '</td>';
                             echo '<td>' . $row['date_fin_cg'] . '</td>';
+                            echo '<td>' . $row['statut_cg'] . '</td>';
                             echo ' <form  action="actualiser.php "method="post">';
                             echo '  <td>
-                                        <input type="submit" name="accepter" id="btnA" value="">
-                                        <input type="submit" name="reffuser" id="btnR" value="">
+                                        <input type="submit" name="accepter" id="btnA" value="" onclick="verifcoment(event)">
+                                        <input type="submit" name="reffuser" id="btnR" value="" onclick="verifcoment(event)">
                                         <label class="label_coment" for="comment">Commentaire</label>
+                                        <input type="text" name="comment" class="comment">
                                         <p class="error"></p>
-                                        <input type="hidden" name="idc" value="' . $row["id_emp"] . '">
+                                        <input type="hidden" name="idc" value="' . $row["id_cg"] . '">
                                     </td>';
                             echo '</form>';
                             echo '</tr>';
@@ -61,9 +61,22 @@ echo "<p>Bonjour " . $_SESSION['Auth']['nom_emp'] . " </p>";
                         </tr>
                     </tbody>
                 </table>
-            </section>
+            </div>
         </main>
     </form>
 </body>
+
+<script>
+    //ajouter un evenemnt pour boutton entrer pour vérifier l'email et le mot de passe 
+    function verifcoment(event) {
+        let commentField = document.querySelectorAll('.comment');
+        if (commentField.value.length < 20) {
+            event.preventDefault();
+            alert('Le champ commentaire doit contenir au moins 20 caractères.');
+        }
+
+    }
+</script>
+
 
 </html>
