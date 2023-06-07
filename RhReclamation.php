b5eaf725a0bfe87d74ea0cd90d0e42ed266cdea3
kassimi youssef <youssef.kassimi-etu@etu.univh2c.ma>
2023-06-07 16:10:10 +0100

    bbb

--- RhReclamation.php
+++ RhReclamation.php
@@ -40,14 +40,14 @@ echo "<p>Bonjour " . $_SESSION['Auth']['nom_emp'] . " </p>";
                         <?php
                         require 'connect.php';
                         $state = 'en cours';
-                        $result = $bdd->query("SELECT * FROM `reclamation`, `employe` WHERE reclamation.ID_EMP = employe.ID_EMP  AND reclamation.RESPO_DESTINE='0'");
+                        $result = $bdd->query("SELECT * FROM `reclamation`, `employee` WHERE reclamation.id_emp = employee.id_emp  AND reclamation.respo_destine='1'");
                         while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-                            echo '<tr class="clickable-row" data-href="http://localhost/projet_web/TraiterReclamation.php/?idrec=' . $row["ID_REC"] . '&dest=' . $row["RESPO_DESTINE"] . '&emp=' . $row["ID_EMP"] . '">';
-                            echo '<td>' . $row['NOM_EMP'] . '</td>';
-                            echo '<td>' . $row['PRENOM_EMP'] . '</td>';
+                            echo '<tr class="clickable-row" data-href="http://localhost/projet_web/TraiterReclamation.php/?idrec=' . $row["id_rec"] . '&dest=' . $row["respo_destine"] . '&emp=' . $row["id_emp"] . '">';
+                            echo '<td>' . $row['nom_emp'] . '</td>';
+                            echo '<td>' . $row['prenom_emp'] . '</td>';
                             echo ' <form  action="TraiterReclamation.php "method="post">';
                             echo '  <td>
-                                        <input type="hidden" name="idc" value="' . $row["ID_EMP"] . '">
+                                        <input type="hidden" name="idc" value="' . $row["id_emp"] . '">
                                     </td>';
                             echo '</form>';
                             echo '</tr>';
